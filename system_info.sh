987cea4256898b7e16208a01ac4d1532410ed76b
Alexey Naumov <alexeynaumov@Lenovo-Legion-5-15ARH05H-267a6435.localdomain>
2023-09-25 00:28:22 +0700

    Create a bash-script with system information

---
+++ system_info.sh
@@ -0,0 +1,106 @@
+#!/bin/bash
+
+# Получаем текущую дату
+current_date=$(date)
+
+# Получаем имя пользователя
+user_name=$(whoami)
+
+# Получаем доменное имя ПК (hostname)
+domain_name=$(hostname)
+
+# Получаем информацию о процессоре
+cpu_info=$(lscpu)
+cpu_model=$(echo "$cpu_info" | grep "Model name" | awk -F: '{print $2}' | xargs)
+cpu_architecture=$(echo "$cpu_info" | grep "Architecture" | awk -F: '{print $2}' | xargs)
+cpu_max_freq=$(echo "$cpu_info" | grep "Max MHz" | awk -F: '{print $2}' | xargs)
+cpu_curr_freq=$(echo "$cpu_info" | grep "Current MHz" | awk -F: '{print $2}' | xargs)
+cpu_cores=$(echo "$cpu_info" | grep "CPU(s):" | awk -F: '{print $2}' | xargs)
+cpu_threads=$(echo "$cpu_info" | grep "Thread(s) per core" | awk -F: '{print $2}' | xargs)
+cpu_usage=$(top -bn1 | grep "Cpu(s)" | awk '{print $2 + $4}')
+
+# Получаем информацию о оперативной памяти
+memory_info=$(free -h)
+memory_l1=$(lscpu | grep "L1d cache" | awk -F: '{print $2}' | xargs)
+memory_l2=$(lscpu | grep "L2 cache" | awk -F: '{print $2}' | xargs)
+memory_l3=$(lscpu | grep "L3 cache" | awk -F: '{print $2}' | xargs)
+memory_total=$(echo "$memory_info" | grep "Mem:" | awk '{print $2}')
+memory_available=$(echo "$memory_info" | grep "Mem:" | awk '{print $7}')
+
+# Получаем информацию о жестком диске
+disk_info=$(df -h)
+disk_total=$(echo "$disk_info" | grep "/$" | awk '{print $2}')
+disk_available=$(echo "$disk_info" | grep "/$" | awk '{print $4}')
+num_partitions=$(echo "$disk_info" | grep -c "^/dev/")
+
+# Получаем информацию о каждом разделе
+partition_info=$(df -h)
+partition_list=$(df -h | grep "^/dev/")
+
+# Получаем информацию о корневой директории
+root_partition=$(df -h / | tail -1)
+root_mounted=$(echo "$root_partition" | awk '{print $1}')
+root_unallocated=$(parted -l | grep -A1 "^Model: " | tail -1 | awk '{print $3}')
+
+# Получаем информацию о SWAP
+swap_info=$(free -h | grep "Swap:")
+swap_total=$(echo "$swap_info" | awk '{print $2}')
+swap_available=$(echo "$swap_info" | awk '{print $3}')
+
+# Получаем информацию о сетевых интерфейсах
+network_interfaces=$(ip addr show)
+num_interfaces=$(echo "$network_interfaces" | grep -c "^[0-9]:")
+
+# Выводим полученные данные
+echo "Дата: $current_date"
+echo "Имя учетной записи: $user_name"
+echo "Доменное имя ПК: $domain_name"
+echo "Процессор:"
+echo "Модель: $cpu_model"
+echo "Архитектура: $cpu_architecture"
+echo "Тактовая частота максимальная: $cpu_max_freq MHz"
+echo "Тактовая частота текущая: $cpu_curr_freq MHz"
+echo "Количество ядер: $cpu_cores"
+echo "Количество потоков на одно ядро: $cpu_threads"
+echo "Загрузка процессора: $cpu_usage%"
+echo "Оперативная память:"
+echo "Cache L1: $memory_l1"
+echo "Cache L2: $memory_l2"
+echo "Cache L3: $memory_l3"
+echo "Всего: $memory_total"
+echo "Доступно: $memory_available"
+echo "Жесткий диск:"
+echo "Всего: $disk_total"
+echo "Доступно: $disk_available"
+echo "Количество разделов: $num_partitions"
+
+# Выводим информацию о каждом разделе
+echo "Информация о разделах:"
+echo "$partition_list"
+
+# Выводим информацию о корневой директории
+echo "Смонтировано в корневую директорию /: $root_mounted"
+echo "Объём неразмеченного пространства: $root_unallocated"
+echo "SWAP всего: $swap_total"
+echo "SWAP доступно: $swap_available"
+
+# Выводим информацию о сетевых интерфейсах
+echo "Сетевые интерфейсы:"
+echo "Количество сетевых интерфейсов: $num_interfaces"
+echo "$network_interfaces" | grep "^[0-9]:" | while read -r line
+do
+  interface_name=$(echo "$line" | awk '{print $2}' | rev | cut -c 2- | rev)
+  interface_mac=$(ip link show "$interface_name" | awk '/ether/ {print $2}')
+  interface_ip=$(echo "$line" | awk '/inet/ {print $2}')
+  interface_standard=$(ethtool "$interface_name" 2>/dev/null | grep "Supported link modes" | awk -F: '{print $2}' | xargs)
+  interface_max_speed=$(ethtool "$interface_name" 2>/dev/null | grep "Speed" | awk -F: '{print $2}' | xargs)
+  interface_actual_speed=$(ethtool "$interface_name" 2>/dev/null | grep "Duplex" | awk -F: '{print $2}' | xargs)
+  
+  echo "Интерфейс: $interface_name"
+  echo "MAC: $interface_mac"
+  echo "IP: $interface_ip"
+  echo "Стандарт связи: $interface_standard"
+  echo "Максимальная скорость соединения: $interface_max_speed"
+  echo "Фактическая скорость соединения: $interface_actual_speed"
+done
+
